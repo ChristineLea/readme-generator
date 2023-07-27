38770f5d1480718acdad5432540a8c13e1b13248
Christine Lea <christine87@live.com.au>
2023-07-27 17:09:01 +1000

    add generateMarkdown function to module

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,36 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-	return `# ${data.title}
+	const license = renderLicenseSection(data.license);
 
+	return `# ${data.title}
+  \n ${license}
+  \n ## Description 
+  \n ${data.description} 
+  \n ## Table of Contents
+  \n - [1. Installation](#1-installation)
+  \n - [2. Usage](#2-usage)
+  \n - [3. Contributing](#3-contributing)
+  \n - [4. Tests](#4-tests)
+  \n - [5. Questions](#5-questions)
+  \n - [6. Authors](#6-authors)
+  \n - [7. License](#-license)
+  \n ## 1. Installation
+  \n ${data.installation}
+  \n ## 2. Usage 
+  \n ${data.usage}
+  \n ## 3. Contributing 
+  \n ${data.contributing}
+  \n ## 4. Tests
+  \n ${data.tests}
+  \n ## 5. Questions
+  \n For any questions or support with using this application, 
+  \n 1. Submit an issue through my [GitHub Profile](${data.github})
+  \n 2. Send me an email at ${data.email}
+  \n ## 6. Authors
+  \n ${data.authors}
+  \n ## 7. License
+  \n This project is covered under ${data.license} license.
 `;
 }
 
